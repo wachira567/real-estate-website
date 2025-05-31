6f285ae4e918c770379a6d80f6bc3bb1e583b59a
wachira567 <wachiramboche@gmail.com>
2025-05-31 20:55:49 +0300

    website

---
+++ client/src/app/(nondashboard)/search/[id]/PropertyOverview.tsx
@@ -0,0 +1,103 @@
+import { useGetPropertyQuery } from "@/state/api";
+import { MapPin, Star } from "lucide-react";
+import React from "react";
+
+const PropertyOverview = ({ propertyId }: PropertyOverviewProps) => {
+  const {
+    data: property,
+    isError,
+    isLoading,
+  } = useGetPropertyQuery(propertyId);
+
+  if (isLoading) return <>Loading...</>;
+  if (isError || !property) {
+    return <>Property not Found</>;
+  }
+
+  return (
+    <div>
+      {/* Header */}
+      <div className="mb-4">
+        <div className="text-sm text-gray-500 mb-1">
+          {property.location?.country} / {property.location?.state} /{" "}
+          <span className="font-semibold text-gray-600">
+            {property.location?.city}
+          </span>
+        </div>
+        <h1 className="text-3xl font-bold my-5">{property.name}</h1>
+        <div className="flex justify-between items-center">
+          <span className="flex items-center text-gray-500">
+            <MapPin className="w-4 h-4 mr-1 text-gray-700" />
+            {property.location?.city}, {property.location?.state},{" "}
+            {property.location?.country}
+          </span>
+          <div className="flex justify-between items-center gap-3">
+            <span className="flex items-center text-yellow-500">
+              <Star className="w-4 h-4 mr-1 fill-current" />
+              {property.averageRating.toFixed(1)} ({property.numberOfReviews}{" "}
+              Reviews)
+            </span>
+            <span className="text-green-600">Verified Listing</span>
+          </div>
+        </div>
+      </div>
+
+      {/* Details */}
+      <div className="border border-primary-200 rounded-xl p-6 mb-6">
+        <div className="flex justify-between items-center gap-4 px-5">
+          <div>
+            <div className="text-sm text-gray-500">Monthly Rent</div>
+            <div className="font-semibold">
+              ${property.pricePerMonth.toLocaleString()}
+            </div>
+          </div>
+          <div className="border-l border-gray-300 h-10"></div>
+          <div>
+            <div className="text-sm text-gray-500">Bedrooms</div>
+            <div className="font-semibold">{property.beds} bd</div>
+          </div>
+          <div className="border-l border-gray-300 h-10"></div>
+          <div>
+            <div className="text-sm text-gray-500">Bathrooms</div>
+            <div className="font-semibold">{property.baths} ba</div>
+          </div>
+          <div className="border-l border-gray-300 h-10"></div>
+          <div>
+            <div className="text-sm text-gray-500">Square Feet</div>
+            <div className="font-semibold">
+              {property.squareFeet.toLocaleString()} sq ft
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Summary */}
+      <div className="my-16">
+        <h2 className="text-xl font-semibold mb-5">About {property.name}</h2>
+        <p className="text-gray-500 leading-7">
+          {property.description}
+          Discover modern living at SK Apartments, where comfort and convenience converge 
+          in the heart of Wangige, Kenya. Situated on Kirawa Road, our brand-new community 
+          offers stylish one, two, and three-bedroom residences crafted for contemporary 
+          lifestyles. Each apartment features sleek granite countertops, energy-efficient 
+          appliances, built-in wardrobes, and in-unit laundry facilities for ultimate practicality. 
+          Enjoy peace of mind with our reliable backup generator, ensuring uninterrupted power 
+          for your daily needs.
+
+          Relax in the serene ambiance of our beautifully landscaped gardens and courtyards, ideal 
+          for unwinding or hosting guests. Located on Kirawa Road, SK Apartments is just minutes from 
+          Wangige’s vibrant market, offering fresh produce and local goods, and provides easy access to 
+          Thika Road and the Nairobi-Nakuru Highway, connecting you to Nairobi’s CBD, Westlands, and The 
+          Village Market for shopping and dining.
+
+          SK Apartments is in Kiambu County, Wangige, a thriving community known for its blend of urban 
+          accessibility and suburban charm. Nearby, you’ll find essential services such as healthcare 
+          facilities and schools, ensuring convenience for families and professionals alike. Contact us 
+          today to tour SK Apartments and experience a lifestyle of comfort and modern elegance on Kirawa Road.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyOverview;
